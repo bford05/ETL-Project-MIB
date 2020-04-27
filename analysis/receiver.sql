bdf738b8fd23bfa9656dbf0d475b3207d8e28cb7
Lion <supermylesj@aol.com>
2020-04-27 13:58:38 -0400

    rusher&receiver.sql

---
+++ analysis/receiver.sql
@@ -0,0 +1,7 @@
+create table receiver(
+receiver_id int,
+team_id int,
+player_id int,
+rec_position varchar(10),
+rec_yards dec
+);
---
+++ analysis/rusher.sql
@@ -0,0 +1,8 @@
+create table rusher(
+rush_id int,
+team_id int,
+player_id int,
+rush_position varchar(10),
+rush_yards dec,
+rush_td int
+);
