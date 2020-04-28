ee6241eb2a82abf578510e679be8434d0a7a57df
Brian Ford <59347919+bford05@users.noreply.github.com>
2020-04-27 22:04:33 -0400

    update final version of etl ipynb and nfl_db.sql

--- analysis/nfl_db.sql
+++ analysis/nfl_db.sql
@@ -38,33 +38,46 @@ create table draft (
 );
 
 create table rusher(
-rush_id int,
-team_id int,
-player_id int,
-rush_position varchar(10),
-rush_yards dec,
-rush_td int
+	rush_id int NOT NULL,
+	team_id int NOT NULL,
+	player_id int NOT NULL,
+	rush_position varchar(10) NOT NULL,
+	rush_yards decimal NOT NULL,
+	rush_td int NOT NULL
 );
 
 
 create table receiver(
-receiver_id int,
-team_id int,
-player_id int,
-rec_position varchar(10),
-rec_yards dec
+	receiver_id int NOT NULL,
+	team_id int NOT NULL,
+	player_id int NOT NULL,
+	rec_position varchar(10) NOT NULL,
+	rec_yards decimal NOT NULL
 );
 
-Create table combine (
-player_id int NOT NULL,
-hand_size decimal NOT NULL,
-forty_yd_time decimal NOT NULL,
-bench_press_rep int NOT NULL,
-CONSTRAINT pk_combine PRIMARY KEY (player_id)
+create table combine (
+	player_id int NOT NULL,
+	hand_size decimal NOT NULL,
+	forty_yd_time decimal NOT NULL,
+	bench_press_rep int NOT NULL
 );
 
 create table passer (
+	team_id int NOT NULL,
+	player_id int NOT NULL,
+	pass_id int NOT NULL,
+	pass_position varchar (225) NOT NULL,
+	pass_outcomes varchar (225) NOT NULL,
+	pass_length int NOT NULL,
+	pass_attempt int NOT NULL,
+	pass_completion int NOT NULL,
+	pass_touchdown int NOT NULL,
+	pass_interception int NOT NULL
+);
 
-)
 
-
+create table unique_player (
+	player_id int NOT NULL,
+	full_name character varying (60) NOT NULL,
+	CONSTRAINT pk_unique_player PRIMARY KEY (player_id)
+);
